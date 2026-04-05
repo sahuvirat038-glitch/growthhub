661679ed008d56dbf0fdbe079648777434205a87
Ronnie <sahuvirat038@gmail.com>
2026-04-05 12:51:47 -0400

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -12,11 +12,20 @@ from dotenv import load_dotenv
 from app.tasks import scheduler
 from app.database import engine, Base
 import app.models
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
